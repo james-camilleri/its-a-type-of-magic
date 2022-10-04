ab0c484353f341dc4479b1c709b7644d73adb150
James Camilleri <james@james.mt>
2022-10-05 00:51:35 +0100

    Add more images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import Slide from '$lib/components/layout/Slide.svelte'
+  import CodeExample from './CodeExample.svelte'
 
   import TypoLFA from '$assets/typo_lfa.mp4'
   import CloseUps from '$assets/close-ups.mp4'
@@ -18,6 +19,13 @@
   import Spotlight from '$assets/images/spotlight.png'
   import WhatIsDada from '$assets/images/what-is-dada.jpg'
   import WizardOfOz from '$assets/images/wizard-of-oz.png'
+  import LFA from '$assets/images/lfa.jpg'
+  import SonicBloom from '$assets/images/sonic-bloom.webp'
+  import TedX from '$assets/images/ted-x.jpg'
+  import TheatreTypes from '$assets/images/theatre-types.png'
+  import TypoWithSoftware from '$assets/images/typo-with-software.jpg'
+  import EyeCatcher from '$assets/images/eye-catcher.png'
+  import ObliterationRoom from '$assets/images/obliteration-room.png'
 </script>
 
 <section data-background-color="black" />
@@ -38,14 +46,13 @@
 
 <Slide title="construction" />
 <Slide video={CloseUps} loop={true} />
-<Slide image={PriorArt} />
-<Slide image={OnJournalism2} />
-<Slide image={Prototype_1} />
-<Slide image={Prototype_2} />
-<section
-  data-background-iframe="https://stately.ai/viz/embed/71a899bf-d43a-4f95-b687-0ddaf0cc4386?mode=viz&panel=code&showOriginalLink=1&readOnly=1&pan=1&zoom=1&controls=1"
-  data-background-interactive="true"
+<Slide image={PriorArt} caption={['Harvey Moon, Alfredo Salazar-Caro']} />
+<Slide
+  image={OnJournalism2}
+  caption={['Julian Koschwitz', '"On Journalism #2 Typewriter" (1992)']}
 />
+<Slide image={Prototype_1} />
+<!-- <Slide image={Prototype_2} /> -->
 <section
   data-background-iframe="https://gallery.autodesk.com/projects/172467/assets/664311/embed"
   data-background-interactive="true"
@@ -54,6 +61,16 @@
   data-background-iframe="https://gallery.autodesk.com/projects/172467/assets/664318/embed"
   data-background-interactive="true"
 />
+<section
+  data-background-iframe="https://stately.ai/viz/embed/71a899bf-d43a-4f95-b687-0ddaf0cc4386?mode=viz&panel=code&showOriginalLink=1&readOnly=1&pan=1&zoom=1&controls=1"
+  data-background-interactive="true"
+/>
+<!-- TODO: Get the code in somehow. -->
+<!-- <CodeExample /> -->
+<!-- <section
+  data-background-iframe="https://github.com/james-camilleri/typewriter-dmx/blob/main/index.ts"
+  data-background-interactive="true"
+/> -->
 <!-- <section
   data-background-iframe="https://www.masswerk.at/elizabot/eliza.html"
   data-background-interactive="true"
@@ -61,21 +78,59 @@
 <Slide title="bloomsbury theatre" />
 <Slide image={Bloomsbury} />
 <Slide image={WizardOfOz} />
-<Slide image={MechanicalTurk} />
+<section
+  data-background-iframe="https://typo.digital/live/performance/bloomsbury"
+  data-background-interactive="true"
+/>
+<Slide
+  image={MechanicalTurk}
+  caption={['Mechanical Turk or Automaton Chess Player', '(18th century)']}
+/>
 <Slide image={Spotlight} />
-<Slide image={EECummings} />
+<Slide
+  image={EECummings}
+  caption={['e. e. cummings', 'Tulips & Chimneys (1922)']}
+/>
 
 <Slide title="here east" />
+<Slide image={Crits_1} />
+<Slide image={Crits_2} />
+
 <Slide title="london festival of architecture" />
+<Slide image={LFA} />
+
 <Slide title="TEDx goodenough college" />
+<Slide image={TedX} />
+
 <Slide title="ars electronica festival" />
 <Slide title="thesis" subtitle="evina kipeni-thalassinou" />
 <Slide subtitle="interactive performance" />
 <Slide subtitle="co-creation" />
+<Slide
+  image={ObliterationRoom}
+  caption={['Yayoi Kusama', 'Obliteration room']}
+/>
 <Slide subtitle="the &quot;gaze&quot; of the artwork" />
+<Slide
+  image={EyeCatcher}
+  caption={['(Charlie) Zhang Lin', 'Eye Catcher (2014)']}
+/>
 <Slide subtitle="typo as therapist" />
+<Slide image={SonicBloom} caption={['Yuri Suzuki', 'Sonic Bloom (2022)']} />
 <Slide subtitle="conclusion" />
-<Slide title="next steps" />
+<Slide
+  image={TheatreTypes}
+  caption={['Different types of theater', 'Pinterest (n.d.)']}
+/>
 
-<Slide image={Crits_1} />
-<Slide image={Crits_2} />
+<Slide title="next steps" />
+<section
+  data-background-iframe="https://typo.digital/live/test/poetry"
+  data-background-interactive="true"
+/>
+<Slide image={TypoWithSoftware} />
+<!--
+caption={['Paul Smith']}
+caption={['F. Queiroz', 'Typewriter art (2018)']}
+caption={['M. Messinger', 'Typewriter Stories (2019)']}
+caption={['Rafael Lozano-Hemmer', 'Atmospheric Memory (2021)']} -->
