c880b9b3f8aaf60e23db0d78f82c60d23c3b7f20
James Camilleri <james@james.mt>
2022-11-02 17:35:48 +0000

    Add summary component

---
+++ src/routes/Summary.svelte
@@ -0,0 +1,60 @@
+<section>
+  <p>
+    Typo is an interactive experience with a vintage typewriter that has a life
+    of its own<span class="punctuation">.</span> Animated by robotics and
+    artificial intelligence<span class="punctuation">,</span> it writes poetry
+    through dialogue with its human audience<span class="punctuation">.</span>
+  </p>
+  <p>
+    The mechanical typewriter is an anachronism from a bygone era<span
+      class="punctuation">,</span
+    >
+    yet it remains a potent symbol of mechanisation and progress in art and literature<span
+      class="punctuation">.</span
+    >
+    By splicing together the digital and the analogue<span class="punctuation"
+      >,</span
+    >
+    we breathe new life into the machine and turn it into a primary performer<span
+      class="punctuation">,</span
+    >
+    capable of responding to its surroundings and creating of its own accord<span
+      class="punctuation">.</span
+    >
+    Through its exploration of AI<span class="punctuation">,</span> Typo invites
+    us to question the idea of originality<span class="punctuation">,</span> and
+    to grapple with our opinions of algorithmically-generated art when compared
+    to pieces crafted entirely by human minds and hands<span class="punctuation"
+      >.</span
+    >
+    When poems are a product of user input<span class="punctuation">,</span>
+    complex software<span class="punctuation">,</span> and a vast digital
+    library amounts of existing literature<span class="punctuation">,</span> who
+    is the artist?
+  </p>
+  <p>
+    Typo offers moments of wonder and playfulness<span class="punctuation"
+      >,</span
+    >
+    as audiences interact with an enigmatic machine in a variety of live performances
+    and experience the often vague and overly-technical world of AI in a tangible
+    way<span class="punctuation">.</span> No longer a simple tool<span
+      class="punctuation">,</span
+    >
+    Typo gains a role as an autonomous co-creator alongside its audience<span
+      class="punctuation">.</span
+    >
+  </p>
+</section>
+
+<style>
+  p {
+    margin-bottom: 1em;
+    font-size: 0.5em;
+    text-align: left;
+  }
+
+  .punctuation {
+    color: var(--primary);
+  }
+</style>
