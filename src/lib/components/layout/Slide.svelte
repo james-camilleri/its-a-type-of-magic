a9df77d0bc4a98347631c4fbbf67320226894f5c
James Camilleri <james@james.mt>
2022-11-02 17:36:39 +0000

    Clean up Slide component

--- src/lib/components/layout/Slide.svelte
+++ src/lib/components/layout/Slide.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte'
+  import Caption from './Caption.svelte'
 
   export let title = ''
   export let subtitle = ''
@@ -10,12 +11,32 @@
   export let loop = false
   export let image = ''
   export let video = ''
+  export let iframe = ''
   export let caption: string | string[] | undefined = undefined
 
   let element: HTMLElement
   let displayTitle = ''
   let displaySubtitle = false
   let animated = false
+
+  const dataAttributes = image
+    ? {
+        'data-background-image': image,
+        'data-background-size': contain ? 'contain' : 'cover',
+      }
+    : video
+    ? {
+        'data-background-video': video,
+        'data-background-video-loop': loop,
+      }
+    : iframe
+    ? {
+        'data-background-iframe': iframe,
+        'data-background-interactive': true,
+      }
+    : {
+        'data-auto-animate': animate || undefined,
+      }
 
   function random(min: number, max: number) {
     return Math.random() * (max - min) + min
@@ -53,48 +74,16 @@
   })
 </script>
 
-{#if image}
-  <section
-    bind:this={element}
-    data-background-image={image}
-    data-background-size={contain ? 'contain' : 'cover'}
-  >
-    {#if caption && caption.length > 0}
-      <div class="caption">
-        <span
-          >{typeof caption === 'string' ? caption : caption[0]}<span
-            class="underscore">_</span
-          ></span
-        >
-        {#if Array.isArray(caption)}
-          {#each caption.slice(1) as text}
-            <span>{text}</span>
-          {/each}
-        {/if}
-      </div>
-    {/if}
-  </section>
-{:else if video}
-  <section
-    bind:this={element}
-    data-background-video={video}
-    data-background-video-loop={loop}
-  />
-{:else}
-  <section
-    bind:this={element}
-    class:small
-    data-auto-animate={animate || undefined}
-  >
-    {#if title}
-      <h1 class:large>{displayTitle}</h1>
-    {/if}
-    {#if displaySubtitle}
-      <p class:subtitle-only={!title}>{subtitle}</p>
-    {/if}
-    <slot />
-  </section>
-{/if}
+<section bind:this={element} {...dataAttributes} class:small>
+  {#if title}
+    <h1 class:large>{displayTitle}</h1>
+  {/if}
+  {#if displaySubtitle}
+    <p class:subtitle-only={!title}>{subtitle}</p>
+  {/if}
+
+  <Caption {caption} />
+</section>
 
 <style lang="scss">
   .large {
@@ -107,23 +96,6 @@
 
   p {
     margin-top: var(--lg);
-  }
-
-  .caption {
-    position: fixed;
-    right: -250px;
-    bottom: 3rem;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem 3rem 1rem 1rem;
-    font-size: 1.2rem;
-    font-weight: 300;
-    text-align: right;
-    background: rgb(255 255 255 / 90%);
-
-    > span:first-child {
-      font-weight: 400;
-    }
   }
 
   .underscore {
